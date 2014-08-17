b385b90ad856f4ab716b70f74d9b3e67882ef04f
rrtigga <rrtigga@ucdavis.edu>
2014-08-16 18:24:58 -0700

    624

--- app/src/main/java/com/ProjectShop/app/ImageZoomActivity.java
+++ app/src/main/java/com/ProjectShop/app/ImageZoomActivity.java
@@ -4,13 +4,19 @@ package com.ProjectShop.app;
  * Created by Spicycurryman on 8/16/14.
  */
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 import com.gridview.shirts.ImageAdapter;
 
@@ -23,6 +29,8 @@ public class ImageZoomActivity extends Activity {
     private TextView XLarge;
 
     View previousView;
+   private Button purchasebutton;
+
 
 
     @Override
@@ -30,6 +38,14 @@ public class ImageZoomActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zoom_image);
 
+
+        SpannableString s = new SpannableString("ProjectShop");
+        s.setSpan(new TypefaceSpan(this, "actionman.ttf"), 0, s.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+// Update the action bar title with the TypefaceSpan instance
+        ActionBar actionBar = getActionBar();
+        actionBar.setTitle(s);
 
         Small = (TextView) findViewById(R.id.Small);
         Small.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/actionmanshaded.ttf"));
@@ -44,9 +60,6 @@ public class ImageZoomActivity extends Activity {
         XLarge.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/actionmanshaded.ttf"));
 
 
-
-
-
         // get intent data
         Intent i = getIntent();
 
@@ -56,31 +69,83 @@ public class ImageZoomActivity extends Activity {
 
         ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
         imageView.setImageResource(imageAdapter.mLargeThumbIds[position]);
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView previousText = (TextView) previousView;
+                TextView curText = (TextView) v;
+                // If the clicked view is selected, do nothing
+                if (curText.isSelected()) {
+                    //curText.setSelected(false);
+                    //curText.setTextColor(getResources().getColor(R.color.red_highlight));
+                } else { // If this isn't selected, deselect  the previous one (if any)
+                    if (previousText != null && previousText.isSelected()) {
+                        previousText.setSelected(false);
+                        previousText.setTextColor(getResources().getColor(R.color.black));
+                    }
+                    curText.setSelected(true);
+                    curText.setTextColor(getResources().getColor(R.color.red_highlight));
+                    previousView = v;
+                }
+
+            }
+
+
+
+
+        };
+
+
+        findViewById(R.id.Small).setOnClickListener(clickListener);
+        findViewById(R.id.Medium).setOnClickListener(clickListener);
+        findViewById(R.id.Large).setOnClickListener(clickListener);
+        findViewById(R.id.XLarge).setOnClickListener(clickListener);
+
+
+        purchasebutton = (Button) findViewById(R.id.purchasebutton);
+
+
+        purchasebutton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // Create custom dialog object
+                final Dialog dialog = new Dialog(ImageZoomActivity.this);
+                // Include dialog.xml file
+                dialog.setContentView(R.layout.payment);
+                // Set dialog title
+                dialog.setTitle("Payment Information");
+
+
+
+                dialog.show();
+
+                Button submitbutton = (Button) dialog.findViewById(R.id.btnsubmit);
+                // if decline button is clicked, close the custom dialog
+                submitbutton.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Close dialog
+                        dialog.dismiss();
+                    }
+                });
+
+
+            }
+
+        });
+
+
+
+
+
     }
 
 
-    View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            TextView previousText = (TextView) previousView;
-            TextView curText = (TextView) v;
-            // If the clicked view is selected, do nothing
-            if (curText.isSelected()) {
-                //curText.setSelected(false);
-                //curText.setTextColor(getResources().getColor(R.color.red_highlight));
-            } else { // If this isn't selected, deselect  the previous one (if any)
-                if (previousText != null && previousText.isSelected()) {
-                    previousText.setSelected(false);
-                    previousText.setTextColor(getResources().getColor(R.color.black));
-                }
-                curText.setSelected(true);
-                curText.setTextColor(getResources().getColor(R.color.red_highlight));
-                previousView = v;
-            }
 
-        }
 
-    };
 
 
 
